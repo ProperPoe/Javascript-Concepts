4a147de2e48dea8c979916904529ab413270f3ad
Potency <proper@ProperPoe>
2023-07-31 09:31:11 -0600

    Added more Javascript concepts

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function displaySquare(fn){
     console.log("Square is " + fn(5)) //passing functions into functions
 }
 
-displaySquare(square)
+//displaySquare(square)
 //IIFE(Immediately Invoked Function Expressions)
 (function yards(num){
     console.log(num * 3)
@@ -349,3 +349,74 @@ result()
 
 console.log("stop")
 
+//Promise Polyfill
+function PromisePolyfill(executor){
+    let onResolve, 
+        onReject,
+        isFullFilled = false,
+        isRejected = false,
+        isCalled = false,
+        value;
+
+    function resolve(val) {
+        isFullFilled = true;
+        value = val;
+
+        if(typeof onResolve === "function"){
+            onResolve(val);
+            isCalled = true;
+        }
+
+        
+    }
+    
+    function reject(val) {
+        isRejected = true;
+        value = val;
+
+        if(typeof onReject === "function"){
+            onReject(val);
+            isCalled = true;
+        }
+    }
+
+    this.then = function(callback){
+        onResolve = callback;
+
+        if (isFullFilled && !isCalled){
+            isCalled = true;
+            onResolve(value);
+        }
+
+        return this;
+    };
+
+    this.catch = function (callback){
+        onReject = callback;
+
+        if (isRejected && !isCalled){
+            isCalled = true;
+            onReject(value);
+        }
+
+        return this;
+    }
+    try {
+        executor(resolve, reject)
+    } catch (error) {
+        console.error(error)
+    }
+
+    
+}
+
+const examplePromise = new PromisePolyfill((resolve, reject) => {
+    setTimeout(()=>{
+        resolve(2);
+    }, 1000);
+})
+
+examplePromise
+    .then((res)=> {
+        console.log(res);
+    }).catch((err) => console.error(err))
